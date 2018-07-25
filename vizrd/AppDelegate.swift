87bf47387fee3325cd3e9ab3f2ac34b5b36e4fed
Max Desiatov <max.desiatov@gmail.com>
2018-07-25 14:29:28 +0100

    Initial Commit

---
+++ vizrd/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  vizrd
+//
+//  Created by Max Desiatov on 25/07/2018.
+//  Copyright © 2018 Max Desiatov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ vizrd/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  vizrd
+//
+//  Created by Max Desiatov on 25/07/2018.
+//  Copyright © 2018 Max Desiatov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
